456079d704308b1d37ebb5e13ccf3a3206525cb7
Levina Tani <edelaida@gmail.com>
2025-04-24 16:54:55 +0300

    status label

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
-import ActiveLabel from "@/components/active-label";
-import NotActiveLabel from "@/components/not-active-label";
+import StatusLabel, { Status } from "./components/status-label";
 
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold underline" >Home page</h1>
-      <ActiveLabel>active</ActiveLabel>
-      <NotActiveLabel>not active</NotActiveLabel>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
     </main>       
   );
 }
