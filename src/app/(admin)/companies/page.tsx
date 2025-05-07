5ed5adcde40149590bd6ae95486dfd2e5fd5bb97
Levina Tani <edelaida@gmail.com>
2025-05-07 08:42:42 +0300

    intercepting routes

--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import Header from '@/app/components/header';
-import Toolbar from '@/app/components/toolbar';
-import SearchInput from '@/app/components/search-input';
-import AddCompanyButton from '@/app/components/add-company-button';
 import CompanyTable from '@/app/components/company-table';
 import CompanyRow from '@/app/components/company-row';
 import { Status } from '@/app/components/status-label';
@@ -11,13 +7,7 @@ export interface PageProps {}
 
 export default function Page({}: PageProps) {
   return (
-    <>
-      <Header>Companies</Header>
-      <main>
-        <Toolbar action={<AddCompanyButton />}>
-          <SearchInput />
-        </Toolbar>
-        <CompanyTable>
+       <CompanyTable>
           <CompanyRow
             id={1}
             category="Products"
@@ -27,8 +17,6 @@ export default function Page({}: PageProps) {
             country="USA"
             joinedDate="02.19.2023"
           />
-        </CompanyTable>
-      </main>
-    </>
+        </CompanyTable>     
   );
 }
